d9a4916659246f157b170474a653166630e8bd48
keleko34 <gguidero@dataart.com>
2016-03-10 11:18:58 +0100

    update button

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 var buttonContainer = document.querySelector('.buttonContainer');
 
 var _button = CreateButton()
-.content("Click Button");
+.text("Click Button");
 
 
 _button.call(_button,buttonContainer);
