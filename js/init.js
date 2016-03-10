a0129ffb35b5eefeffadda24e6735e42de7e2f19
keleko34 <gguidero@dataart.com>
2016-03-10 16:16:38 +0100

    added Slider component

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 var buttonContainer = document.querySelector('.buttonContainer')
-  , RadioContainer = document.querySelector('.radioContainer');
+  , RadioContainer = document.querySelector('.radioContainer')
+  , SliderContainer = document.querySelector('.sliderContainer');
 
 var _button = CreateButton()
 .text("Click Button")
@@ -8,6 +9,8 @@ var _button = CreateButton()
 var _radio = CreateRadio()
 .text('Toggle');
 
+var _slider = CreateSlider().min(100).max(10000);
 
 _button.call(_button,buttonContainer);
 _radio.call(_radio,RadioContainer);
+_slider.call(_slider,SliderContainer);
