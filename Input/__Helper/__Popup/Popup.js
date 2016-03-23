da30e0596af7839128bad405b73833854601304d
keleko34 <gguidero@dataart.com>
2016-03-23 16:46:34 +0100

    add popup pos logic

--- Input/__Helper/__Popup/Popup.js
+++ Input/__Helper/__Popup/Popup.js
@@ -9,6 +9,19 @@ define(function(){
     function Input__Helper__Popup(node)
     {
       var _popup = node.querySelector('.Input__Helper__Popup');
+      if(!_popup)
+      {
+        _popup = node.appendChild(document.createElement('div'));
+      }
+
+      if(_type === 'hover')
+      {
+        if(node.offsetParent.innerHTML === node.parentNode.innerHTML && node.style.position === undefined)
+        {
+          node.style.position = 'relative';
+        }
+      }
+
     }
 
     return Input__Helper__Popup;
