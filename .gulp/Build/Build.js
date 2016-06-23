3a004fa98ac6eb80d4600fb849501adaefdc6756
keleko34 <gregory.guidero@gmail.com>
2016-06-23 16:34:32 +0200

    set css

--- .gulp/Build/Build.js
+++ .gulp/Build/Build.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp')
   , prompt = require('gulp-prompt')
   , file = require('gulp-file')
+  , inject = require('gulp-inject')
+  , sort = require('sort-stream')
+  , replace = require('gulp-replace')
+  , file = require('gulp-file')
+  , closureCompiler = require('gulp-closure-compiler')
   , fs = require('fs');
 
 var config = global.gulp.config;
