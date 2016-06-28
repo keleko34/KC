a07682660f084dfd99621ba2b45c4039fc862fc0
keleko34 <gregory.guidero@gmail.com>
2016-06-28 23:55:39 +0200

    create now works with any templates

--- .gulp/Create/Create.js
+++ .gulp/Create/Create.js
@@ -48,50 +48,14 @@ module.exports = function(){
   }
 
   function Command(res){
-    var jsFile = fs.readFileSync('./' + config.tasks.Create.base + '/Templates/Component/Component.js','utf8')
-        .replace(/(Component)/g,res.component),
+    fs.readdirSync('./' + config.tasks.Create.base + '/Templates/Component').forEach(function(k,i){
+      var template = fs.readFileSync('./' + config.tasks.Create.base + '/Templates/Component/'+k,'utf8')
+      .replace(/(Component)/g,res.component)
+      .replace(/(Description)/g,res.description),
+          ext = k.split('.')[(k.split('.').length-1)];
 
-        vmFile = fs.readFileSync('./' + config.tasks.Create.base + '/Templates/Component/Component.vm.js','utf8')
-        .replace(/(Component)/g,res.component),
-
-        cssFile = fs.readFileSync('./' + config.tasks.Create.base + '/Templates/Component/Component.css','utf8')
-        .replace(/(Component)/g,res.component),
-
-        readmeFile = fs.readFileSync('./' + config.tasks.Create.base + '/Templates/Component/Component.md','utf8')
-        .replace(/(Component)/g,res.component)
-        .replace(/(Description)/g,res.description)
-
-    file('./'+res.component+".js",jsFile,{src:true})
-    .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
-
-    file('./'+res.component+".vm.js",vmFile,{src:true})
-    .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
-
-    file('./'+res.component+".css",cssFile,{src:true})
-    .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
-
-    file('./'+res.component+".html","",{src:true})
-    .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
-
-    file('./README.md',readmeFile,{src:true})
-    .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
-
-    /* This stage we strip away require and inline the html and css and inline concat the viewmodel */
-    file('./dev/'+res.component+".js","",{src:true})
-    .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
-
-    /* This stage we minify and have dev for easy debug testing */
-    file('./qa/'+res.component+".js","",{src:true})
-    .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
-    file('./qa/'+res.component+".min.js","",{src:true})
-    .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
-
-    /* This stage is pure minified version for final check, auto documentation is made for methods and properties to fill */
-    file('./stage/'+res.component+".min.js","",{src:true})
-    .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
-
-    /* This stage is the final minified product, Hooray! */
-    file('./prod/'+res.component+".min.js","",{src:true})
-    .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
+      file('./'+res.component+'.' + ext,template,{src:true})
+      .pipe(gulp.dest('./'+ config.components.base + '/' + res.component));
+    });
   };
 };
