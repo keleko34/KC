075350cd5396d6f78afcb1f369a9f8592cdb9dca
keleko34 <gregory.guidero@gmail.com>
2016-07-12 13:23:13 +0200

    auto loading of pages and components

--- .core/init.js
+++ .core/init.js
@@ -2,7 +2,7 @@ define(['./override', './router'],function(override, router){
 
   override.loader();
   /* Here we should require the main page Header, Content, Footer */
-  require(['Sections/Upload/Upload'],function(Upload){
+  require([],function(){
     ko.applyBindings(router.bindings)
   });
 });
--- .core/override.js
+++ .core/override.js
@@ -1,9 +1,67 @@
 define([],function(){
 
+  function loadComponent(routes, callback){
+    var query = parseQuery(location.search);
+
+    routes.map(function(k,i){
+      if(query.env !== undefined){
+        return k+(k.indexOf('.js') > -1 ? '' : '.js')+'?env='+query.env;
+      }
+      return k;
+    })
+    .forEach(function(k,i){
+        if(!require.defined(k)){
+            require([k],function(vm){
+              routes.splice(routes.indexOf(k),1);
+              if(routes.length < 1){
+                callback();
+              }
+            }, function(err){
+              console.log(k,' does not exist in but is refrenced in its routes');
+            })
+        }
+    });
+  }
+
+  function searchComponents(check,callback){
+    var elroutes = Object.keys(element_routes),
+        routes = [];
+
+    for(var x=0;x<elroutes.length;x++){
+      var componentroutes = Object.keys(element_routes[elroutes[x]]);
+      for(var i=0;i<componentroutes.length;i++){
+        if(check(componentroutes[i])){
+          routes.push(element_routes[elroutes[x]][componentroutes[i]]);
+        }
+      }
+    }
+    callback(routes);
+  }
+
   /* This overrides the components loader so that an element is attached the viewmodel and the viewmodel is atached to the element */
   function loader(){
-
     ko.components.loaders.unshift({
+      getConfig: function(name, callback) {
+        if(!ko.components.isRegistered(name)){
+
+          searchComponents(function(component){
+            return component === name;
+          },function(routes){
+            if(routes.length > 0){
+              loadComponent(routes,function(){
+                ko.components.defaultLoader.getConfig(name, callback);
+              });
+            }
+            else{
+              console.error('You have not set the component path inside elements.config for ',name);
+              ko.components.defaultLoader.getConfig(name, callback);
+            }
+          });
+        }
+        else{
+          ko.components.defaultLoader.getConfig(name, callback);
+        }
+      },
       loadViewModel: function (name, viewModelConfig, callback) {
         if (typeof viewModelConfig === "function") {
           var viewModelConstructor = {
@@ -19,35 +77,18 @@ define([],function(){
       },
       loadTemplate: function (name, templateConfig, callback) {
         if (typeof templateConfig === 'string'){
-          var found = [],
-              cb = function(){
+          searchComponents(function(component){
+            return (templateConfig.indexOf('</'+component+'>') > -1 && !ko.components.isRegistered(component));
+          },function(routes){
+            if(routes.length > 0){
+              loadComponent(routes,function(){
                 ko.components.defaultLoader.loadTemplate(name, templateConfig, callback);
-              },
-              query = parseQuery(location.search);
-          Object.keys(element_routes).forEach(function(t,x){
-            Object.keys(element_routes[t]).forEach(function(k,i){
-              if(templateConfig.indexOf('</'+k+'>') > -1){
-
-                if(query.env !== undefined){
-                  element_routes[t][k] = element_routes[t][k]+(element_routes[t][k].indexOf('.js') > -1 ? '' : '.js')+'?env='+query.env;
-                }
-                if(!require.defined(element_routes[t][k])){
-                    found.push(k);
-                    require([element_routes[t][k]],function(vm){
-                      found.splice(found.indexOf(k),1);
-                      if(found.length < 1){
-                        cb();
-                      }
-                    }, function(err){
-                      console.log(element_routes[t][k],' does not exist in ',t, ' but is refrenced in its routes');
-                    })
-                }
-              }
-            });
+              });
+            }
+            else{
+              ko.components.defaultLoader.loadTemplate(name, templateConfig, callback);
+            }
           });
-          if(found.length < 1){
-            ko.components.defaultLoader.loadTemplate(name, templateConfig, callback);
-          }
         } else if (templateConfig.fromUrl) {
           require(['text!'+templateConfig.fromUrl],function(cp){
             ko.components.defaultLoader.loadTemplate(name, cp, callback);
