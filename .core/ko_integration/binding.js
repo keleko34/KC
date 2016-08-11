14bbb5e5eb38be7a6bf2c70a498e0dfbcda54601
keleko34 <gregory.guidero@gmail.com>
2016-08-11 23:34:44 +0200

    bind private vars

--- .core/ko_integration/binding.js
+++ .core/ko_integration/binding.js
@@ -4,9 +4,36 @@
  * Created By Keleko34
  */
 
-function integrateBindings(){
+  /* Changes we need to keep track of on a component:
+   * Text: html(properties), appendChild, removeChild
+   * Events: on(properties|attributes), addEventListener
+   * Attributes: setAttribute, removeAttribute, title, href, value !(id|class)
+   * NOTE: We do not track class or id, they are problematic props.
+   */
+
+/* All text props */
+var _textEvents = ['innerHTML','outerHTML','textContent','innerText','outerText'],
+    _onEvents = Object.keys(HTMLUnknownElement.prototype).filter(function(k){
+      return (k.indexOf('on')  === 0);
+    }),
+    _attrEvents = function(el){
+      return Array.prototype.slice.call(el.attributes)
+        .map(function(k){return k.name;})
+        .filter(function(k){return (k !== 'data-bind' && k !== 'id' && k !== 'class')});
+    }
+
+function integrateBindings(el){
+  el.ko_override = {};
+  el.ko_override.binds = {};
+
 
 }
+
+integrateBindings.getClassValue(prop,value){
+
+}
+
+
 
 define([],function(){return integrateBindings});
 define('integrateBindings',function(){return integrateBindings;});
